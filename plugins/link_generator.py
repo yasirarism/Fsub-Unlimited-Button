c2e9d7005f2704ee4b6a32fde613e6685b870bcb
Yasir Aris M <git@yasir.id>
2026-01-16 21:05:30 +0700

    Change button text from 'Share Link' to 'File Link'

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -57,7 +57,7 @@ async def batch(client: Client, message: Message):
         [
             [
                 InlineKeyboardButton(
-                    "🔁 Share Link", url=f"https://telegram.me/share/url?url={link}"
+                    "📁 File Link", url=link
                 )
             ]
         ]
@@ -96,7 +96,7 @@ async def link_generator(client: Client, message: Message):
         [
             [
                 InlineKeyboardButton(
-                    "🔁 Share Link", url=f"https://telegram.me/share/url?url={link}"
+                    "📁 File Link", url=link
                 )
             ]
         ]
